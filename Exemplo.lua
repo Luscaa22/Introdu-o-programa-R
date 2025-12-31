05be2453c5617596254937c719f03867642b2c04
𝐋𝐮𝐬𝐜𝐚𝐚 <lucas.martinho@viasapienslab.com.br>
2025-12-31 02:34:12 -0300

    Refactor function integrity checks and add new hooks

--- Exemplo.lua
+++ Exemplo.lua
@@ -12,9 +12,7 @@ end
 function SL:ProtectFunction(f)
     if type(f) ~= "function" then return end
     if self.Data[f] then return end
-
     local i = info(f)
-
     self.Data[f] = {
         ref = f,
         what = i and i.what,
@@ -30,34 +28,14 @@ function SL:VerifyFunctionIntegrity(f)
     if type(f) ~= "function" then return false end
     local d = self.Data[f]
     if not d then return false end
-
     local i = info(f)
     if not i then return true end
-
-    if d.iscclosure and not (iscclosure and iscclosure(f)) then
-        return true
-    end
-
-    if d.what ~= i.what then
-        return true
-    end
-
-    if d.source ~= i.source then
-        return true
-    end
-
-    if d.linedefined ~= i.linedefined then
-        return true
-    end
-
-    if d.nups ~= i.nups then
-        return true
-    end
-
-    if getfenv(f) ~= d.env then
-        return true
-    end
-
+    if d.iscclosure and not (iscclosure and iscclosure(f)) then return true end
+    if d.what ~= i.what then return true end
+    if d.source ~= i.source then return true end
+    if d.linedefined ~= i.linedefined then return true end
+    if d.nups ~= i.nups then return true end
+    if getfenv(f) ~= d.env then return true end
     return false
 end
 
@@ -70,6 +48,25 @@ function SL:RevertHook(f)
         end
     end
     return true
+end
+
+function SL:ForceHook(f, crash)
+    if type(f) ~= "function" then return end
+    hookfunction(f, newcclosure(function(...)
+        task.spawn(crash, "HTTP Spy ativo")
+        while true do end
+    end))
+end
+
+function SL:ScanUI(crash)
+    for _, v in pairs(game.CoreGui:GetDescendants()) do
+        if v:IsA("ScreenGui") then
+            local n = v.Name:lower()
+            if n:find("spy") or n:find("http") or n:find("remote") or n:find("simple") then
+                crash("Spy detectado")
+            end
+        end
+    end
 end
 
 return setmetatable({}, SL)
