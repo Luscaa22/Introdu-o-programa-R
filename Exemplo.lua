036626c54a7a49cfd61c166d45312428dc1809ab
𝐋𝐮𝐬𝐜𝐚𝐚 <lucas.martinho@viasapienslab.com.br>
2025-12-31 02:04:02 -0300

    Refactor SL:Hook and add UnhookAll method

--- Exemplo.lua
+++ Exemplo.lua
@@ -1,9 +1,22 @@
 local SL = {}
+
 SL.Hooks = {}
+SL.Fingerprints = {}
+
+function SL:Fingerprint(func)
+    return tostring(func)
+end
 
 function SL:Hook(func, callback)
-    if type(func) ~= "function" then return end
-    if self.Hooks[func] then return self.Hooks[func] end
+    if type(func) ~= "function" or type(callback) ~= "function" then
+        return
+    end
+
+    if self.Hooks[func] then
+        return self.Hooks[func]
+    end
+
+    self.Fingerprints[func] = self:Fingerprint(func)
 
     local old
     old = hookfunction(func, function(...)
@@ -23,14 +36,35 @@ function SL:RevertHook(func)
     if old then
         hookfunction(func, old)
         self.Hooks[func] = nil
+        self.Fingerprints[func] = nil
         return true
     end
     return false
 end
 
 function SL:VerifyFunctionIntegrity(func)
-    if type(func) ~= "function" then return false end
+    if type(func) ~= "function" then
+        return false
+    end
+
+    local fp = self.Fingerprints[func]
+    if not fp then
+        return false
+    end
+
+    return tostring(func) ~= fp
+end
+
+function SL:IsHooked(func)
     return self.Hooks[func] ~= nil
+end
+
+function SL:UnhookAll()
+    for func, old in pairs(self.Hooks) do
+        hookfunction(func, old)
+    end
+    self.Hooks = {}
+    self.Fingerprints = {}
 end
 
 return SL
